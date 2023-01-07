d4b09b270d0990c3048a485f544b966f22b0136e
JamilyaAlmaz <jamulinushka@gmail.com>
2023-01-07 11:03:22 +0600

    edited clientPage

--- test/specs/clients.spec.js
+++ test/specs/clients.spec.js
@@ -4,12 +4,30 @@ const ClientPage = require('../pageobjects/examples/clientPage')
 
 describe('Clients test', () => {
     beforeEach( async()=> {
-        await browser.url(browser.options.baseUrl + 'login');
+        await LoginPage.open();
         await LoginPage.doLogin();
+        await LoginPage.open();
     })
 
     it ('should create new client', async () => {
+        //уже тут шаги открытия формы клиента и заполнения 
+        //первый способ
         await ClientPage.createClientButton.click();
+        await ClientPage.surnameField.setValue("Тестовая фамилия");
+        await ClientPage.firstNameField.setValue("Тестовое имя");
+
+        //второй способ
+        let testingUser = {
+            surname: "Тестовая фамилия"
+        }
+
+        await ClientPage.fillForm(testingUser)
+        //окончание второго способа
+
         await browser.pause(5000);
+    })
+
+    it('remain test cases', async () => {
+
     })
 })
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -2,7 +2,7 @@ const LoginPage = require('../pageobjects/examples/LoginPage')
 
 describe ('Login page on Navigator CRM', () => {
     beforeEach('Setup browser', async() =>{
-        await browser.url('http://167.114.201.175:5000/login/');
+        await LoginPage.open();
     })
     afterEach('Clear browser', async()=>{
         await browser.reloadSession();
@@ -18,38 +18,4 @@ describe ('Login page on Navigator CRM', () => {
         await expect(browser).toHaveUrlContaining('login');
 
     })
-
-
-
-
-
-        /*
-        await LoginPage.passwordField.setValue('Admin@Navi');
-        await browser.pause(5000);
-
-        //нажать на кнопку вход
-        await LoginPage.submitButton.click();
-        await browser.pause(5000);
-
-
-        await expect(browser).toHaveUrlContaining('clients')
-        })
-         */
-
-
 })
-
-
-        //ввести логин
-        //Варианты селекторов на поле Логин:
-        //input[name="userName"] 
-        //input[type="text"]
-        //[type="text"]
-        // супер точный путь например: form > div > input[type="text"] 
-
-         //ввести пароль
-        //Варианты селекторов на поле Пароль:
-        //input[name="password"]
-        //input[type="password"]
-        //[type="password"]
-        //form > div > input[type="password"]
